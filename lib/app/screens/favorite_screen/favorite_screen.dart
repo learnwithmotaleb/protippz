697abffdc4cb302782fffa0335aa064c47e95303
Nadimpg <bsmrstunadimcse@gmail.com>
2025-02-11 16:04:34 +0600

    complete

--- lib/app/screens/favorite_screen/favorite_screen.dart
+++ lib/app/screens/favorite_screen/favorite_screen.dart
@@ -158,8 +158,10 @@ class FavoriteScreen extends StatelessWidget {
 
                       ///================================Player=========================
                       return CustomPlayerCard(
-                        imageUrl:
-                            "${ApiUrl.netWorkUrl}${playerData.player?.playerImage ?? ""}",
+
+                        imageUrl: playerData.player?.playerImage?.isNotEmpty == true
+                            ? "${ApiUrl.netWorkUrl}${playerData.player?.playerImage}"
+                            : AppConstants.profileImage,
                         name: playerData.player?.name ?? "",
                         team: playerData.player?.team?.name ?? "",
                         position: playerData.player?.position ?? "",
@@ -193,8 +195,10 @@ class FavoriteScreen extends StatelessWidget {
 
                       ///=============================Team==========================
                       return CustomTeamCard(
-                        imageUrl:
-                            "${ApiUrl.netWorkUrl}${teamData.team?.teamLogo ?? ""}",
+
+                        imageUrl: teamData.team?.teamLogo?.isNotEmpty == true
+                            ? "${ApiUrl.netWorkUrl}${teamData.team?.teamLogo}"
+                            : AppConstants.profileImage,
                         name: teamData.team?.name ?? "",
                         sport: teamData.team?.league?.sport ?? "",
                         onTap: () {
