59733700745c5c569c10b79bec6d2f49cfae5657
masum <promotionbd321@gmail.com>
2025-06-16 21:19:35 +0600

    fix issue

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 
-import 'package:device_preview/device_preview.dart';
-import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_stripe/flutter_stripe.dart';
@@ -19,13 +17,14 @@ void main() {
   di.dependencies();
   Stripe.publishableKey = AppConstants.stripePublishableKey;
   runApp(
-      DevicePreview(
-        enabled: !kReleaseMode,
-        builder: (context) =>
-        const MyApp(), // Wrap your app
-      )
+      // DevicePreview(
+      //   enabled: !kReleaseMode,
+      //   builder: (context) =>
+      //   const MyApp(), // Wrap your app
+      // )
+      const MyApp()
       );
-  const MyApp();
+
 }
 
 
